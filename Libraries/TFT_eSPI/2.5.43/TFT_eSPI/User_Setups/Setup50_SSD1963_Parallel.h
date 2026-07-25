ad3a50fb6f2e034c94a634e01482220a2b2c6276
Kevin Guest <github@bionicbone.plus.com>
2026-07-25 11:58:52 +0100

    Part of initial commit

---
+++ Libraries/TFT_eSPI/2.5.43/TFT_eSPI/User_Setups/Setup50_SSD1963_Parallel.h
@@ -0,0 +1,132 @@
+//                            USER DEFINED SETTINGS
+//   Set driver type, fonts to be loaded, pins used and SPI control method etc.
+//
+//   See the User_Setup_Select.h file if you wish to be able to define multiple
+//   setups and then easily select which setup file is used by the compiler.
+//
+//   If this file is edited correctly then all the library example sketches should
+//   run without the need to make any more changes for a particular hardware setup!
+#define USER_SETUP_ID 50
+// ##################################################################################
+//
+// Section 0. Call up the right driver file and any options for it
+//
+// ##################################################################################
+
+// Use ESP32 Parallel interface
+#define TFT_PARALLEL_8_BIT
+
+// Only define one driver, the other ones must be commented out
+//#define SSD1963_480_DRIVER    // 272 x 480 display
+//#define SSD1963_800_DRIVER    // 480 x 800 display
+//#define SSD1963_800ALT_DRIVER // Alternative 480 x 800 display
+#define SSD1963_800BD_DRIVER    // 480 x 800 display sourced from https://www.buydisplay.com/7-tft-screen-touch-lcd-display-module-w-ssd1963-controller-board-mcu
+
+//#define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
+#define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
+
+//// ##################################################################################
+////
+//// Section 1a. Define the pins that are used to interface with the display here
+////
+//// ##################################################################################
+//
+//// ESP32 pins used
+//#define TFT_CS   33  // Chip select control pin
+//#define TFT_DC   15  // Data Command control pin - must use a pin in the range 0-31
+//#define TFT_RST  32  // Reset pin
+//
+//#define TFT_WR    4  // Write strobe control pin - must use a pin in the range 0-31
+//#define TFT_RD    2
+//
+//#define TFT_D0   12  // Must use pins in the range 0-31 for the data bus
+//#define TFT_D1   13  // so a single register write sets/clears all bits
+//#define TFT_D2   26
+//#define TFT_D3   25
+//#define TFT_D4   5   //17
+//#define TFT_D5   23  //16
+//#define TFT_D6   27
+//#define TFT_D7   14
+
+// ##################################################################################
+//
+// Section 1b. Define the pins that are used to interface with the display here
+//
+// ##################################################################################
+
+// ESP32S3 pins used
+#define TFT_CS   -1 // tied to GND, N/A for parallel  // Chip select control pin
+#define TFT_DC   48 // Data Command control pin - must use a pin in the range 0-31
+#define TFT_RST  -1 // tied to ESP32S3 RST Pin  // Reset pin
+
+#define TFT_WR   3  // Write strobe control pin - must use a pin in the range 0-31
+#define TFT_RD   -1 // Display read not used in my project, but display can be read so may need a pin later
+
+#define TFT_D0	 8  // Must use pins in the range 0-31 or alternatively 32-48
+#define TFT_D1   18 // so a single register write sets/clears all bits.
+#define TFT_D2   17 // Pins can be randomly assigned, this does not affect
+#define TFT_D3   16 // TFT screen update performance.
+#define TFT_D4   15
+#define TFT_D5   7
+#define TFT_D6   6
+#define TFT_D7   5
+
+#define TOUCH_CAPACITIVE
+
+#ifdef TOUCH_CAPACITIVE
+//#define I2C_FT5206  0x38
+//#define SDA_FT5206  1
+//#define SCL_FT5206  4
+//#define INT_FT5206  2
+#else
+//#define TOUCH_CS 46    // Chip select pin (T_CS) of touch screen
+
+#endif
+
+
+
+// ##################################################################################
+//
+// Section 2. Define the way the DC and/or CS lines are driven (ESP8266 only)
+//
+// ##################################################################################
+
+
+// ##################################################################################
+//
+// Section 3. Define the fonts that are to be used here
+//
+// ##################################################################################
+
+// Comment out the #defines below with // to stop that font being loaded
+// The ESP8366 and ESP32 have plenty of memory so commenting out fonts is not
+// normally necessary. If all fonts are loaded the extra FLASH space required is
+// about 17Kbytes. To save FLASH space only enable the fonts you need!
+
+#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+
+// Comment out the #define below to stop the SPIFFS filing system and smooth font code being loaded
+// this will save ~20kbytes of FLASH
+#define SMOOTH_FONT
+
+// ##################################################################################
+//
+// Section 4. Not used
+//
+// ##################################################################################
+
+
+// ##################################################################################
+//
+// Section 5. Other options
+//
+// ##################################################################################
+
+
---
+++ hardware/Readme.md
@@ -0,0 +1,13 @@
+Copyright (C) 2026 Kevin Guest (TheBionicBone)
+
+This documentation describes Open Hardware and is licensed under the
+CERN-OHL-W v2 or any later version.
+
+You may redistribute and modify this documentation and make products using it
+under the terms of the CERN-OHL-W v2 (https://ohwr.org/cern-ohl-w-v2). This
+documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING
+OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR PURPOSE.
+Please see the CERN-OHL-W v2 for applicable conditions.
+
+Source Location: https://github.com/bionicbone/Workshop-Environment-Monitor
+SPDX-License-Identifier: CERN-OHL-W-2.0
